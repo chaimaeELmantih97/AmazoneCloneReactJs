f8b026811fc6a9849d78d4559878c79458514b66
chaimaeElmantih <elmantih.chaimae1997@gmail.com>
2021-06-17 10:18:37 +0100

    checkout start

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-
+import Checkout from "./Checkout";
 
 function App() {
   return (
-     <Router> 
-       <div className="app">
-     <Switch>
-       <Route path="/checkout">
-         {/* what er render */}
-          <Header /> 
-         <h1>checkout page</h1>
-       </Route>
-        <Route path="/login">
-         {/* what er render */}
-         <h1>login page</h1>
-       </Route>
-       <Route path="/">
-         {/* what er render */}
-         <Header /> 
-         <Home />
-       </Route>
-     </Switch>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            {/* what er render */}
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            {/* what er render */}
+            <h1>login page</h1>
+          </Route>
+          <Route path="/">
+            {/* what er render */}
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
-   
   );
 }
 
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { useStateVAlue } from "./StateProvider";
-
+import "./checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 function Checkout() {
   const [{ basket }] = useStateVAlue();
   return (
     <div className="checkout">
       <img
-        className="checkout_img"
+        className="checkout_ad"
         src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
         alt=""
         srcset=""
       />
+      {basket?.length === 0 ? (
+        <div>
+          <h2>your Shopping Basket is empty</h2>
+          <p>
+            You have no items in your basket. To buy one or more items, click
+            "Add to basket" next to the item.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2 className="checkout_title">Tour Shopping Basket</h2>
+          {/* list out all the checkout products */}
+          {basket.map((item) => (
+            <CheckoutProduct />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
